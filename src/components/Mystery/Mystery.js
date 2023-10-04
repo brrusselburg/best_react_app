c4dd3278a163f90101e9267b0bfb3b5e7cf25374
Benji Russelburg <benji.russelburg@reverb.com>
2023-10-04 13:51:56 -0500

    Initial Setup + mystery button

---
+++ src/components/Mystery/Mystery.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Mystery = () => {
+// who knows what this will be! not me
+
+  return (
+    <div className={classes.counter}>
+      <h2 className={classes.header}>Mystery Button</h2>
+      <button
+        className={classes.button}
+        type="button"
+        onClick={incrementCounter}>
+        ??????
+      </button>
+    </div>
+  );
+};
+
+export default Mystery;
